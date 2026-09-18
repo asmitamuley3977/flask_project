25032fd2f968d258e6f3596ea855304257021a16
asmitamuley3977 <asmitamuley95@gmail.com>
2026-09-18 22:39:56 +0530

    task 2.py

--- task 2.py
+++ task 2.py
@@ -30,3 +30,26 @@ try:
 
 except:
     print("Something went wrong!")
+
+
+
+
+
+# OUTPUT :
+Data received successfully!
+
+All Users:
+Leanne Graham - Sincere@april.biz
+Ervin Howell - Shanna@melissa.tv
+Clementine Bauch - Nathan@yesenia.net
+Patricia Lebsack - Julianne.OConner@kory.org
+Chelsey Dietrich - Lucio_Hettinger@annie.ca
+Mrs. Dennis Schulist - Karley_Dach@jasper.info
+Kurtis Weissnat - Telly.Hoeger@billy.biz
+Nicholas Runolfsdottir V - Sherwood@rosamond.me
+Glenna Reichert - Chaim_McDermott@dana.io
+Clementina DuBuque - Rey.Padberg@karina.biz
+
+Users from London:
+
+Process finished with exit code 0
